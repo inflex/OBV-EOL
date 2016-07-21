3d5bc8ec5622382cd9a095952eabb8025b0c5810
Inflex <pldaniels@gmail.com>
2016-07-21 19:42:27 +1000

    Disable imgui.ini writing/generation for Windows

--- src/openboardview/main_dx9.cpp
+++ src/openboardview/main_dx9.cpp
@@ -175,6 +175,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	// extra_fonts/README.txt
 	// for more details)
 	ImGuiIO &io = ImGui::GetIO();
+	io.IniFilename = NULL;
 	int ttf_size;
 	unsigned char *ttf_data = LoadAsset(&ttf_size, ASSET_FIRA_SANS);
 	ImFontConfig font_cfg{};
