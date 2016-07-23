d70dd22c703f27b62f83c37c4d5660c678c30e22
Inflex <pldaniels@gmail.com>
2016-07-23 14:16:16 +1000

    Properly set background colour for linux build

--- src/openboardview/main_opengl.cpp
+++ src/openboardview/main_opengl.cpp
@@ -213,7 +213,6 @@ int main(int argc, char **argv) {
 	io.Fonts->AddFontFromFileTTF(fontpath.c_str(), g.font_size);
 	//	io.Fonts->AddFontDefault();
 
-	ImVec4 clear_color = ImColor(20, 20, 30);
 
 	// Main loop
 	bool done             = false;
@@ -221,6 +220,9 @@ int main(int argc, char **argv) {
 
 	// Now that the configuration file is loaded in to BoardView, parse its settings.
 	app.ConfigParse();
+
+	//ImVec4 clear_color = ImColor(20, 20, 30);
+	ImVec4 clear_color = ImColor(app.m_colors.backgroundColor);
 
 	/*
 	 * If we've asked to load a file from the command line
